a17f7f333125c350432ac5028adf071e664bcc09
Jake Kwon <jake.kwon@central1.com>
2019-06-12 17:24:13 -0700

    Add delete task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ const { src, dest} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const del = require('delete');
 
 exports.default = function() {
+    console.log('-------- default ---------');
     return src('./src/samples/*.js')
         .pipe(babel())
         .pipe( src('./vendor/*.js') )
@@ -13,4 +15,20 @@ exports.default = function() {
             extname: '.min.js'
         }))
         .pipe(dest('./output/minified'))
+}
+
+// exports.delete = async function() {
+//     console.log('-------- delete ---------');
+//     del(['./output'], function(){
+//         console.log('./output deleted');
+//     })
+// }
+exports.delete = function() {
+    console.log('-------- delete ---------');
+    return new Promise((resolve) => {
+        del(['./output'], function(){
+            console.log('./output deleted');
+        });
+        resolve();
+    });
 }
